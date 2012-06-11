7497ec90753308f9adc7df8c605aea9733c9fe02
Matt Courtney <macourtney@gmail.com>
2012-06-11 16:45:39 -0400

    Added a test for the add destination controller

---
+++ test/masques/controller/add_destination/test/add_destination.clj
@@ -0,0 +1,11 @@
+(ns masques.controller.add-destination.test.add-destination
+  (:require [seesaw.core :as seesaw-core]) 
+  (:use clojure.test
+        masques.controller.add-destination.add-destination))
+
+(deftest test-create
+  (let [frame (show nil nil)]
+    (is frame)
+    (is (.isShowing frame))
+    (.doClick (seesaw-core/select frame ["#cancel-button"]))
+    (is (not (.isShowing frame)))))
