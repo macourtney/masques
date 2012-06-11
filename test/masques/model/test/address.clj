a52fd743ab40df9dd16163d01d1900ac50f5fd13
Matt Courtney <macourtney@gmail.com>
2012-06-11 13:17:30 -0400

    Added a test for the address model.

---
+++ test/masques/model/test/address.clj
@@ -0,0 +1,17 @@
+(ns masques.model.test.address
+  (:require [fixtures.identity :as fixtures-identity]) 
+  (:use clojure.test
+        masques.model.address))
+
+(def test-identity (first fixtures-identity/records))
+
+(deftest test-add-address
+  (let [address-id (save-or-update-identity-address test-identity "12345 blah st." "USA" "Virginia" "Reston" "20190")]
+    (is address-id)
+    (let [test-address (get-record address-id)]
+      (is test-address)
+      (is (= test-address (first-identity-address test-identity))))
+    (save-or-update-identity-address test-identity "54321 blah st." "USA" "Virginia" "Reston" "20191")
+    (let [test-address (get-record address-id)]
+      (is test-address)
+      (is (= test-address (first-identity-address test-identity))))))
