1bedf65c642aea57be112aaa4ca5f105608f780c
Matt Courtney <macourtney@gmail.com>
2012-06-11 16:38:41 -0400

    Added tests for the name and phone number models.

---
+++ test/masques/model/test/name.clj
@@ -0,0 +1,20 @@
+(ns masques.model.test.name
+  (:require [fixtures.identity :as fixtures-identity]) 
+  (:use clojure.test
+        masques.model.name))
+
+(def test-identity (first fixtures-identity/records))
+
+(deftest test-add-name
+  (let [name-id (save-or-update-identity-name test-identity "blah")]
+    (is name-id)
+    (try
+      (let [test-name (get-record name-id)]
+        (is test-name)
+        (is (= test-name (first-identity-name test-identity))))
+      (save-or-update-identity-name test-identity "blah2")
+      (let [test-name (get-record name-id)]
+        (is test-name)
+        (is (= test-name (first-identity-name test-identity))))
+      (finally
+        (destroy-record { :id name-id })))))
---
+++ test/masques/model/test/phone_number.clj
@@ -0,0 +1,20 @@
+(ns masques.model.test.phone-number
+  (:require [fixtures.identity :as fixtures-identity]) 
+  (:use clojure.test
+        masques.model.phone-number))
+
+(def test-identity (first fixtures-identity/records))
+
+(deftest test-add-name
+  (let [phone-number-id (save-or-update-identity-phone-number test-identity "blah")]
+    (is phone-number-id)
+    (try
+      (let [test-phone-number (get-record phone-number-id)]
+        (is test-phone-number)
+        (is (= test-phone-number (first-identity-phone-number test-identity))))
+      (save-or-update-identity-phone-number test-identity "blah2")
+      (let [test-phone-number (get-record phone-number-id)]
+        (is test-phone-number)
+        (is (= test-phone-number (first-identity-phone-number test-identity))))
+      (finally
+        (destroy-record { :id phone-number-id })))))
