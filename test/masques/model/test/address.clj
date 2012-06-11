f2ec035d936c5ee3300dc0782248525a0c0b0165
Matt Courtney <macourtney@gmail.com>
2012-06-11 13:32:52 -0400

    The address test will now clean up after itself.

--- test/masques/model/test/address.clj
+++ test/masques/model/test/address.clj
@@ -8,10 +8,13 @@
 (deftest test-add-address
   (let [address-id (save-or-update-identity-address test-identity "12345 blah st." "USA" "Virginia" "Reston" "20190")]
     (is address-id)
-    (let [test-address (get-record address-id)]
-      (is test-address)
-      (is (= test-address (first-identity-address test-identity))))
-    (save-or-update-identity-address test-identity "54321 blah st." "USA" "Virginia" "Reston" "20191")
-    (let [test-address (get-record address-id)]
-      (is test-address)
-      (is (= test-address (first-identity-address test-identity))))))
+    (try
+      (let [test-address (get-record address-id)]
+        (is test-address)
+        (is (= test-address (first-identity-address test-identity))))
+      (save-or-update-identity-address test-identity "54321 blah st." "USA" "Virginia" "Reston" "20191")
+      (let [test-address (get-record address-id)]
+        (is test-address)
+        (is (= test-address (first-identity-address test-identity))))
+      (finally
+        (destroy-record { :id address-id })))))
