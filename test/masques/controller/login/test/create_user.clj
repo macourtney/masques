cea2f30275794c8eb0269b5f85519d3b9079b6fa
Matt Courtney <macourtney@gmail.com>
2012-06-12 15:36:26 -0400

    Added a test for the create user controller.

---
+++ test/masques/controller/login/test/create_user.clj
@@ -0,0 +1,11 @@
+(ns masques.controller.login.test.create-user
+  (:require [seesaw.core :as seesaw-core]) 
+  (:use clojure.test
+        masques.controller.login.create-user))
+
+(deftest test-show
+  (let [frame (show nil)]
+    (is frame)
+    (is (.isShowing frame))
+    (.doClick (seesaw-core/select frame ["#cancel-button"]))
+    (is (not (.isShowing frame)))))
