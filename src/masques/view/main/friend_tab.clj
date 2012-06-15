43edaffef91ba94e966ec37038997fb78934cd3a
Matt Courtney <macourtney@gmail.com>
2012-06-15 15:29:24 -0400

    Updated the friend tab to include the friend text field.

--- src/masques/view/main/friend_tab.clj
+++ src/masques/view/main/friend_tab.clj
@@ -8,7 +8,9 @@
 
 (defn create-friend-list-buttons []
   (seesaw-core/horizontal-panel :items 
-    [ (seesaw-core/button :id :add-friend-button :text (clj-i18n/add)) ]))
+    [ (seesaw-core/button :id :add-friend-button :text (clj-i18n/add))
+      [:fill-h 3]
+      (seesaw-core/button :id :unfriend-button :text (clj-i18n/unfriend))]))
 
 (defn create-friend-list-header-panel []
   (seesaw-core/border-panel
@@ -20,9 +22,34 @@
     (seesaw-core/table :id :friend-table :preferred-size [600 :by 300]
       :model [ :columns friend-table-columns ])))
 
-(defn create []
+(defn create-friend-table-panel []
   (seesaw-core/border-panel
-    :border 9
     :vgap 5
     :north (create-friend-list-header-panel)
     :center (create-friend-list-table)))
+
+(defn create-friend-xml-buttons []
+  (seesaw-core/horizontal-panel :items 
+    [ (seesaw-core/button :id :save-text-button :text (clj-i18n/save))
+      [:fill-h 3]
+      (seesaw-core/button :id :copy-text-button :text (clj-i18n/copy)) ]))
+
+(defn create-friend-xml-header-panel []
+  (seesaw-core/border-panel
+    :west (clj-i18n/friend-text)
+    :east (create-friend-xml-buttons)))
+
+(defn create-friend-xml-text []
+  (seesaw-core/scrollable
+    (seesaw-core/text :id :friend-text :multi-line? true :editable? false :rows 4)))
+
+(defn create-friend-xml-panel []
+  (seesaw-core/border-panel
+    :vgap 5
+    :north (create-friend-xml-header-panel)
+    :center (create-friend-xml-text)))
+
+(defn create []
+  (seesaw-core/vertical-panel
+    :border 9
+    :items [(create-friend-xml-panel) [:fill-v 9] (create-friend-table-panel)]))
