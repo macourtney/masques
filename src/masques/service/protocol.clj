4b17dc80655a4eb8629f572d285e917f02c39299
Matt Courtney <macourtney@gmail.com>
2012-07-10 15:33:09 -0400

    Added the masques service.

---
+++ src/masques/service/protocol.clj
@@ -0,0 +1,27 @@
+(ns masques.service.protocol
+  (:require [clj-i2p.list-service.core :as list-service]
+            [clj-i2p.service-protocol :as service-protocol]))
+
+(defn handle-request [request-map]
+  (condp = (:action request-map)
+    { :error (str "Unknown action: " (:action request-map)) }))
+
+(deftype MasquesService []
+  service-protocol/Service
+  (key [service]
+     :masques-service)
+
+  (name [service]
+    "Masques Service")
+
+  (version [service]
+    "1.0.0-SNAPSHOT")
+
+  (description [service]
+    "This is a service which handles all Masques requests.")
+
+  (handle [service request-map]
+    (handle-request request-map)))
+
+(defn create-service []
+  (MasquesService.))
---
+++ test/masques/service/test/protocol.clj
@@ -0,0 +1,11 @@
+(ns masques.service.test.protocol
+  (:use [masques.service.protocol])
+  (:use [clojure.test])
+  (:require [clj-i2p.service-protocol :as service-protocol]))
+
+(deftest service-test
+  (let [service (create-service)]
+    (is (= :masques-service (service-protocol/key service)))
+    (is (= "Masques Service" (service-protocol/name service)))
+    (is (= "1.0.0-SNAPSHOT" (service-protocol/version service)))
+    (is (= "This is a service which handles all Masques requests." (service-protocol/description service)))))
