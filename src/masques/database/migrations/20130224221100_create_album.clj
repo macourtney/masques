f04a8fe30f5ce273969a2bf91cb564c7372dc0c9
Ted Stein <tstein@innerfile.com>
2013-02-24 22:23:01 -0500

    Schema: album, profile

---
+++ src/masques/database/migrations/20130224221100_create_album.clj
@@ -0,0 +1,18 @@
+(ns masques.database.migrations.20130224221100_create_album
+  (:refer-clojure :exclude [boolean])
+  (:use drift-db.core))
+
+(defn up
+  "Creates the album table in the database."
+  []
+  (create-table :album
+    (id)
+    (timestamp :created_at)
+    (string :name)
+    (int :size_of_all_files)
+    (text :comments)
+  
+(defn down
+  "Drops the album table in the database."
+  []
+  (drop-table :album))
