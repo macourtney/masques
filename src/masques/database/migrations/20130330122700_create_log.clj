5b54346cc2925d790eae4977428711f34557a16c
Ted Stein <tstein@innerfile.com>
2013-03-30 14:40:03 -0400

    Added table for logging.

---
+++ src/masques/database/migrations/20130330122700_create_log.clj
@@ -0,0 +1,19 @@
+(ns masques.database.migrations.20130330122700-create-log
+  (:refer-clojure :exclude [boolean])
+  (:use drift-db.core))
+
+(defn up
+  "Creates the log table in the database."
+  []
+  (create-table :log
+    (id)
+    (date-time :created_at)
+    (string :action)
+    (string :name_space)
+    (text :meta_data)
+    (text :message)))
+  
+(defn down
+  "Drops the log table from the database."
+  []
+  (drop-table :log))
