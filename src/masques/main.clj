212c83ce69e6a24176511748b807983957d52a54
Matt Courtney <macourtney@gmail.com>
2013-04-21 18:58:17 -0400

    Fixed an issue with args not getting propagated.

--- src/masques/main.clj
+++ src/masques/main.clj
@@ -3,12 +3,13 @@
             [masques.controller.data-directory.choose :as data-directory-choose])
   (:gen-class))
 
-(defn choose-action []
-  (core/init-args args)
-  (let [login-frame-ns 'masques.controller.login.login]
-    (require login-frame-ns)
-    ((ns-resolve (find-ns login-frame-ns) 'show))))
+(defn create-choose-action [args]
+  (fn []
+    (core/init-args args)
+    (let [login-frame-ns 'masques.controller.login.login]
+      (require login-frame-ns)
+      ((ns-resolve (find-ns login-frame-ns) 'show)))))
 
 (defn -main
   [& args]
-  (data-directory-choose/show choose-action))
+  (data-directory-choose/show (create-choose-action)))
