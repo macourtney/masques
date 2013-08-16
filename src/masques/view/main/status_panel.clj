214de3b7ebb00890da3bb79623fc510b73bd94b1
Matthew Courtney <macourtney@gmail.com>
2013-08-16 15:42:31 -0400

    Created the recent shares section of the status panel.

--- src/masques/view/main/status_panel.clj
+++ src/masques/view/main/status_panel.clj
@@ -5,12 +5,17 @@
             [seesaw.core :as seesaw-core])
   (:import [java.awt Color]))
 
+(def panel-width 250)
+  
 (def status-background-color (seesaw-color/color 238 238 238))
 (def status-button-font { :name "DIALOG" :style :plain :size 10 })
 
+(def title-color (seesaw-color/color 100 100 100))
+(def title-font { :name "DIALOG" :style :bold :size 14 })
+
 (defn create-update-status []
   (seesaw-core/border-panel
-    :north (seesaw-core/scrollable (seesaw-core/text :id :status-text :multi-line? true :wrap-lines? true :rows 4) :preferred-size [200 :by 75])
+    :north (seesaw-core/scrollable (seesaw-core/text :id :status-text :multi-line? true :wrap-lines? true :rows 4) :preferred-size [panel-width :by 75])
     :south
       (seesaw-core/border-panel
         :west (seesaw-core/button :id :update-status-button :text (term/update-status) :border 0 :background status-background-color :font status-button-font)
@@ -21,7 +26,21 @@
     :vgap 3))
   
 (defn create-recent-shares []
-  (seesaw-core/flow-panel :items ["recent shares"]))
+  (seesaw-core/border-panel
+    :north (seesaw-core/border-panel
+             :west (seesaw-core/label :text (term/recent-shares) :foreground title-color :font title-font)
+             :center (seesaw-core/button :id :inbox-button :text (term/inbox) :border 0 :background status-background-color :font status-button-font)
+             :east (seesaw-core/button :id :filter-button :text (term/filter) :font status-button-font)
+
+             :hgap 3)
+    :south (seesaw-core/scrollable
+             (seesaw-core/listbox :id :recent-shares-listbox
+                 ; :model A ListModel, or a sequence of values with which a DefaultListModel will be constructed.
+                 ; :renderer A cell renderer to use. See (seesaw.cells/to-cell-renderer).
+             )
+             :preferred-size [panel-width :by 200])
+
+    :vgap 3))
 
 (defn create-online-friends []
   (seesaw-core/flow-panel :items ["global actions bar"]))
@@ -32,7 +51,7 @@
     :center (create-recent-shares)
     :south (create-online-friends)
 
-    :vgap 3
+    :vgap 15
     :border (seesaw-border/compound-border
               (seesaw-border/empty-border :thickness 10)
               (seesaw-border/line-border :right 1))))
