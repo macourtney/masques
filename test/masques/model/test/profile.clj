c24380c1f1530b223156705d732b582e65021613
Ted Stein <tstein@innerfile.com>
2013-09-28 12:40:10 -0400

    Added more assertions to test-add-profile

--- test/masques/model/test/profile.clj
+++ test/masques/model/test/profile.clj
@@ -10,16 +10,13 @@
 
 (deftest test-add-profile
   (save profile-record)
-  (is profile-record))
-  ; (is (:id share-record))
-  ; (is (= (:content-type share-record) "message"))
-  ; (is (:message-id share-record))
-  ; (is (not (nil? (:uuid share-record))))
-  ; (is (instance? org.joda.time.DateTime (:created-at share-record))))
+  (is profile-record)
+  (is (:id profile-record))
+  (is (= (:alias profile-record) "Fred"))
+  (is (instance? org.joda.time.DateTime (:created-at profile-record))))
 
 (deftest create-user-profile
   (let [user-profile (create-user "Ted")]
     (is user-profile)
-    (= (:alias user-profile) "Ted")
-    (println user-profile)))
+    (= (:alias user-profile) "Ted")))
 
