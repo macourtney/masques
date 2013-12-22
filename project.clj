99bf9f6dd5656466a2e7999f6319c1495f16f468
Matt Courtney <macourtney@gmail.com>
2013-12-22 13:26:54 -0800

    Updated drift-db to the lastest snapshot.

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
                  [org.clojure/data.xml "0.0.4"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.drift-db/drift-db-h2 "1.1.6-SNAPSHOT"]
+                 [org.drift-db/drift-db-h2 "1.1.7-SNAPSHOT"]
                  [seesaw "1.4.1"]]
 
   :plugins [[lein-libdir "0.1.0"]]
