b0f242dd7ce1a62436c010343261ae80ea0e1647
Ted Stein <tstein@innerfile.com>
2013-12-25 16:44:52 -0500

    Moved file for testing into version control

--- test/masques/model/test/file.clj
+++ test/masques/model/test/file.clj
@@ -3,15 +3,17 @@
   (:use clojure.test
         masques.model.file))
 
+(def avatar-image "./test/support_files/avatar.png")
+(def src "./test/support_files/test.txt")
+(def dest "./test/support_files/test_result.txt")
+
 (deftest test-add-file-record
-  (let [file-record (save { :path "/Users/Ted/masques/avatar.png" })]
+  (let [file-record (save { :path avatar-image })]
     (is file-record)
     (is (:id file-record))
-    (is (= (:path file-record) "/Users/Ted/masques/avatar.png"))
+    (is (= (:path file-record) avatar-image))
     (is (instance? org.joda.time.DateTime (:created-at file-record)))))
 
 (deftest test-copy-file
-  (let [src "/Users/Ted/test.txt"
-        dest "/Users/Ted/test_result.txt"]
-    (copy src dest)))
+  (copy src dest))
 
