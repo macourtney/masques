b5cb0630f4ed447f615ffce2693122e0e546f932
Matt Courtney <macourtney@gmail.com>
2014-07-05 19:16:00 -0700

    Cleaned up some test code by removing unneeded tests.

--- test/masques/controller/group/test/panel.clj
+++ test/masques/controller/group/test/panel.clj
@@ -1,9 +1,7 @@
 (ns masques.controller.group.test.panel
-  (:require [test.init :as test-init]
-            [masques.model.base :as model-base])
+  (:require [test.init :as test-init])
   (:use clojure.test
         masques.controller.group.panel))
 
 (deftest test-create
-  (is (not (model-base/insert-interceptors?)))
   (is (create)))
